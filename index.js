16cead08134835a35c6b22a04aee8bf9fcb56396
EnzoAndSo <grommeshjonathan@gmail.com>
2024-08-07 20:21:32 -0500

    Logos have been tested (manually) now only need to write jest tests

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-//make shapes as modular classes
-//test shapes
-//make inquirer for questions
-//test inquirer
-//make 
-const fs = require('fs')
+//Require necessary pakcages
+const { writeFile } = require('fs')
 const inquirer = require('inquirer')
 const { Circle, Square, Triangle, } = require('./lib/shapes')
 
-function validateColor(color){
-    if ((typeof color) === 'number'){
-        color = `#${color}`
-        console.loge(color, color.length)
-    } else {
-        console.log(typeof color)
-    }
-
-}
-
-function validateResponse(response){
-    
-}
-//begin program
-
-function buildLogo(logo){
+//Call constructors based on shape requested
+function buildLogo(response){
+    let logo
     if (response.shape == 'circle'){
         logo = new Circle(response.text, response.textColor, response.shapeColor)
     } else if (response.shape == 'square'){
@@ -30,8 +13,10 @@ function buildLogo(logo){
     } else if (response.shape == 'triangle'){
         logo = new Triangle(response.text, response.textColor, response.shapeColor)
     }
+    return logo
 }
 
+//Prompt user for logo info
 async function init(){
     const response = await inquirer.prompt([
         {type: 'input', message: 'What is your business acronym? (max of three characters)', name: "text"},
@@ -40,9 +25,11 @@ async function init(){
         {type: 'input', message: 'What color is the shape (by name or hexcode digits)?', name: "shapeColor"},
     ])
     const logo = buildLogo(response)
-    console.log(response)
-    
-    // .then(validateResponse(response))
+    writeFile(`logo.svg`, logo.renderShape(), (err) => {
+        err ? console.lo
+        (err) : console.log('logo generated')
+    })
 }
 
+//Begin program
 init()
